dfd9771dd1d73e7763cecebf4a029894e2a25889
czarjulius <juliusczar.jc@gmail.com>
2020-01-09 19:05:27 +0100

    implemented get methods

--- EmployeeAPI/Controllers/EmployeesController.cs
+++ EmployeeAPI/Controllers/EmployeesController.cs
@@ -1,6 +1,9 @@
 ﻿using EmployeeDataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace EmployeeAPI.Controllers
@@ -15,13 +18,44 @@ namespace EmployeeAPI.Controllers
             }
         }
 
-        public Employee Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using(EmployeeDBEntities entities = new EmployeeDBEntities())
             {
 
-                return entities.Employees.FirstOrDefault(x => x.ID == id);
+                var entity = entities.Employees.FirstOrDefault(x => x.ID == id);
+                if(entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id " + id.ToString() + " is not found");
+                }
             }
+        }
+
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+
+            try {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+
+                    return message;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+            
         }
     }
 }
